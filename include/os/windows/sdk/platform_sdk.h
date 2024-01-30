0e38e0441fad5cf18e7fdd4a4a080b6e1954f84a
VeroFess <coderzeng@hotmail.com>
2024-01-30 20:34:47 +0800

    demo for get player information

--- include/os/windows/sdk/platform_sdk.h
+++ include/os/windows/sdk/platform_sdk.h
@@ -3,13 +3,14 @@
 #include <stdint.h>
 
 namespace Offsets {
-    constexpr int32_t GObjects               = 0x088818E0;
-    constexpr int32_t GWorld                 = 0x089ED4A0;
-    constexpr int32_t AppendString           = 0x02D35860;
-    constexpr int32_t GNames                 = 0x00000000;
-    constexpr int32_t ProcessEvent           = 0x02EE14C0;
-    constexpr int32_t ProcessEventIdx        = 0x0000004C;
-    constexpr int32_t ForceGarbageCollection = 0x04EF0AE0;
+    constexpr int32_t GObjects                 = 0x088818E0;
+    constexpr int32_t GWorld                   = 0x089ED4A0;
+    constexpr int32_t AppendString             = 0x02D35860;
+    constexpr int32_t GNames                   = 0x00000000;
+    constexpr int32_t ProcessEvent             = 0x02EE14C0;
+    constexpr int32_t ProcessEventIdx          = 0x0000004C;
+    constexpr int32_t ForceGarbageCollection   = 0x04EF0AE0;
+    constexpr int32_t LowLevelGetRemoteAddress = 0x013CBA00;
 } // namespace Offsets
 
 uintptr_t GetImageBaseOffset();
--- src/pal_loader_core.cpp
+++ src/pal_loader_core.cpp
@@ -6,17 +6,19 @@
 #include <iostream>
 
 typedef void (*ForceGarbageCollectionType)(SDK::UEngine *engine, bool bForcePurge);
+typedef SDK::FString *(*LowLevelGetRemoteAddressType)(SDK::UIpConnection *connection, bool bAppendPort);
 
 void pal_loader_thread_start() {
     spdlog::info("loading ...");
 
     SDK::InitGObjects();
 
-    SDK::UEngine              *engine                 = nullptr;
-    SDK::UWorld               *world                  = nullptr;
-    SDK::UPalUtility          *utility                = nullptr;
-    SDK::APalGameStateInGame  *stateInGame            = nullptr;
-    ForceGarbageCollectionType ForceGarbageCollection = nullptr;
+    SDK::UEngine                *engine                   = nullptr;
+    SDK::UWorld                 *world                    = nullptr;
+    SDK::UPalUtility            *utility                  = nullptr;
+    SDK::APalGameStateInGame    *stateInGame              = nullptr;
+    ForceGarbageCollectionType   ForceGarbageCollection   = nullptr;
+    LowLevelGetRemoteAddressType LowLevelGetRemoteAddress = nullptr;
 
     for (int i = 0; i < SDK::UObject::GObjects->Num(); i++) {
         SDK::UObject *object = SDK::UObject::GObjects->GetByIndex(i);
@@ -32,11 +34,12 @@ void pal_loader_thread_start() {
 
     world = *reinterpret_cast<SDK::UWorld **>(uintptr_t(GetImageBaseOffset()) + Offsets::GWorld);
 
-    ForceGarbageCollection = reinterpret_cast<ForceGarbageCollectionType>(uintptr_t(GetImageBaseOffset()) + Offsets::ForceGarbageCollection);
-
     utility = SDK::UPalUtility::GetDefaultObj();
 
     stateInGame = utility->GetPalGameStateInGame(world);
+
+    ForceGarbageCollection   = reinterpret_cast<ForceGarbageCollectionType>(uintptr_t(GetImageBaseOffset()) + Offsets::ForceGarbageCollection);
+    LowLevelGetRemoteAddress = reinterpret_cast<LowLevelGetRemoteAddressType>(uintptr_t(GetImageBaseOffset()) + Offsets::LowLevelGetRemoteAddress);
 
     spdlog::info("Unreal::GObjects         = {:x}", uintptr_t(SDK::UObject::GObjects));
     spdlog::info("Unreal::GEngine          = {}", uintptr_t(engine));
@@ -70,8 +73,37 @@ void pal_loader_thread_start() {
             spdlog::info("[CMD::BroadcastChatMessage] {}", message);
         } else if (userInput == "gc") {
             ForceGarbageCollection(engine, true);
-            
+
             spdlog::info("[CMD::ForceGarbageCollection] done");
+        } else if (userInput == "list") {
+            SDK::TArray<SDK::APalCharacter *> player_characters;
+
+            utility->GetAllPlayerCharacters(world, &player_characters);
+
+            if (player_characters.IsValid()) {
+                for (int i = 0; i < player_characters.Num(); i++) {
+                    auto character = static_cast<SDK::APalPlayerCharacter *>(player_characters[i]);
+
+                    auto controller = static_cast<SDK::APlayerController *>(character->GetController());
+
+                    std::string address = std::string("[UNK]");
+
+                    if (controller->NetConnection) {
+                        auto fsaddress = LowLevelGetRemoteAddress(static_cast<SDK::UIpConnection *>(controller->NetConnection), true);
+
+                        if (fsaddress && fsaddress->IsValid() && fsaddress->Num() > 1) {
+                            address = fsaddress->ToString();
+                        }
+                    }
+
+                    auto state = utility->GetPlayerStateByPlayer(character);
+
+                    //TODO: name is also utf16-le, cover it to local
+                    std::string name = state->GetPlayerInfoForMap().PlayerName.ToString();
+
+                    spdlog::info("[CMD::List] GetController = {}, {}", name, address);
+                }
+            }
         } else {
             spdlog::info("[CMD::???] Unknown command");
         }
