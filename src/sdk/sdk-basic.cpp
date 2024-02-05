5487921e8db50be17a0f29882068c98b6d463302
piz.ewing <piz.ewing@gmail.com>
2024-02-05 13:49:49 +0800

    add initialization failure process

--- src/sdk/sdk-basic.cpp
+++ src/sdk/sdk-basic.cpp
@@ -1,13 +1,28 @@
 #include "SDKDirect.h"
+#include <chrono>
+#include <thread>
 
 SDK::UWorld              *SDK::World       = nullptr;
 SDK::APalGameStateInGame *SDK::StateInGame = nullptr;
 
-bool SDK::sdk_init() {
-    World       = *reinterpret_cast<SDK::UWorld **>(uintptr_t(GetImageBaseOffset()) + Offsets::GWorld);
-    StateInGame = GetPalGameStateInGame(World);
+constexpr size_t MAX_TRY_TIMES = 10;
 
-    if (!StateInGame) {
-        return false;
+bool SDK::sdk_init() {
+
+    for (size_t times = 0; times < MAX_TRY_TIMES; times++) {
+        if (World == nullptr) {
+            World = *reinterpret_cast<SDK::UWorld **>(uintptr_t(GetImageBaseOffset()) + Offsets::GWorld);
+        }
+
+        if (World != nullptr) {
+            StateInGame = GetPalGameStateInGame(World);
+            if (StateInGame != nullptr) {
+                return true;
+            }
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
+
+    return false;
 }
